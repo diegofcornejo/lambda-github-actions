f5acadce71381126fe71dc51a442bc1e4c8111de
diegofcornejo <diegof.cornejo@gmail.com>
2021-08-09 14:53:45 -0600

    Inital commit

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Lambda CI with github actions'),
+    };
+    return response;
+};
