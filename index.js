782743f9b2f7292813c641467c002a48aed4a28f
diegofcornejo <diegof.cornejo@gmail.com>
2021-08-11 20:20:45 -0600

    Test lambda creation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Lambda CI with github actions'),
+        body: JSON.stringify('Lambda CI/CD with github actions'),
     };
     return response;
 };
