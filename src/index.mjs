162c0a7e6356e978405a344ca93811bda123d0b7
Diego Cornejo <diegof.cornejo@gmail.com>
2023-11-24 18:11:32 -0600

    chore: Update dependencies and add AWS SDK v3 and fetch API examples

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,28 @@
+import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts";
+
 export const handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('AWS Lambda CI/CD with github actions'),
-    };
-    return response;
+
+	// AWS SDK v3 example
+	const stsClient = new STSClient({ region: 'us-east-1' });
+	const stsCommand = new GetCallerIdentityCommand({});
+	const stsResponse = await stsClient.send(stsCommand);
+	console.log("🚀 ~ file: index.mjs:9 ~ handler ~ stsResponse:", stsResponse)
+
+	// NodeJS 20 native support for fetch API - POKEAPI example
+	const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+	const ditto = await pokeResponse.json();
+	console.log("🚀 ~ file: index.mjs:14 ~ handler ~ ditto:", ditto)
+
+	const res = {
+		message: 'AWS Lambda CI/CD with Github Actions',
+		event,
+		awsSdk: stsResponse,
+		pokeApi: ditto
+	}
+
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify(res)
+	};
+	return response;
 };
