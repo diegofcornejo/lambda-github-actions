0444d89b64abc94be378205edc73403880053734
Diego Cornejo <diegof.cornejo@gmail.com>
2023-11-25 09:34:17 -0600

    Refactor code and add response handling

--- src/index.mjs
+++ src/index.mjs
@@ -2,28 +2,38 @@ import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts";
 
 export const handler = async (event) => {
 
-	// AWS SDK v3 example
-	const stsClient = new STSClient({ region: 'us-east-1' });
-	const stsCommand = new GetCallerIdentityCommand({});
-	const stsResponse = await stsClient.send(stsCommand);
-	console.log("🚀 ~ file: index.mjs:9 ~ handler ~ stsResponse:", stsResponse)
-
-	// NodeJS 20 native support for fetch API - POKEAPI example
-	const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-	const ditto = await pokeResponse.json();
-	console.log("🚀 ~ file: index.mjs:14 ~ handler ~ ditto:", ditto)
-
-	// Return all examples in response
-	const res = {
-		message: 'AWS Lambda CI/CD with Github Actions',
-		event,
-		awsSdk: stsResponse,
-		pokeApi: ditto
+	const done = (statusCode, body) => {
+		return {
+			statusCode,
+			body: JSON.stringify(body) // body must be string
+		};
 	}
 
-	const response = {
-		statusCode: 200,
-		body: JSON.stringify(res) // body must be string
-	};
-	return response;
+	try {
+		// AWS SDK v3 example
+		const stsClient = new STSClient({ region: 'us-east-1' });
+		const stsCommand = new GetCallerIdentityCommand({});
+		const stsResponse = await stsClient.send(stsCommand);
+		console.log("🚀 ~ file: index.mjs:9 ~ handler ~ stsResponse:", stsResponse)
+
+		// NodeJS 20 native support for fetch API - POKEAPI example
+		const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+		const ditto = await pokeResponse.json();
+		console.log("🚀 ~ file: index.mjs:14 ~ handler ~ ditto:", ditto)
+
+		// Return all examples in response
+		const res = {
+			message: 'AWS Lambda CI/CD with Github Actions',
+			event,
+			awsSdk: stsResponse,
+			pokeApi: ditto
+		}
+
+		return done(200, res)
+
+	} catch (error) {
+		console.error("🚀 ~ file: index.mjs:27 ~ handler ~ error", error)
+		return done(500, error)
+	}
+
 };
