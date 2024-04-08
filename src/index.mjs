62f15f13b96209ef76acaef6f751986700db1da0
Diego Cornejo <diegof.cornejo@gmail.com>
2024-04-07 22:15:34 -0600

    Add package.json version to lambda response

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 import { STSClient, GetCallerIdentityCommand } from "@aws-sdk/client-sts";
+import packageJson from "../package.json" assert { type: "json" };
 
 export const handler = async (event) => {
 
@@ -10,20 +11,23 @@ export const handler = async (event) => {
 	}
 
 	try {
+		// Get version from package.json with module import
+		const version = packageJson.version;
+
 		// AWS SDK v3 example
 		const stsClient = new STSClient({ region: 'us-east-1' });
 		const stsCommand = new GetCallerIdentityCommand({});
 		const stsResponse = await stsClient.send(stsCommand);
-		console.log("🚀 ~ file: index.mjs:9 ~ handler ~ stsResponse:", stsResponse)
+		console.log("🚀 ~ file: index.mjs:21 ~ handler ~ stsResponse:", stsResponse)
 
 		// NodeJS 20 native support for fetch API - POKEAPI example
 		const pokeResponse = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
 		const ditto = await pokeResponse.json();
-		console.log("🚀 ~ file: index.mjs:14 ~ handler ~ ditto:", ditto)
+		console.log("🚀 ~ file: index.mjs:26 ~ handler ~ ditto:", ditto)
 
 		// Return all examples in response
 		const res = {
-			message: 'AWS Lambda CI/CD with Github Actions',
+			message: `AWS Lambda CI/CD with Github - version: ${version}`,
 			event,
 			awsSdk: stsResponse,
 			pokeApi: ditto
@@ -32,7 +36,7 @@ export const handler = async (event) => {
 		return done(200, res)
 
 	} catch (error) {
-		console.error("🚀 ~ file: index.mjs:27 ~ handler ~ error", error)
+		console.error("🚀 ~ file: index.mjs:39 ~ handler ~ error", error)
 		return done(500, error)
 	}
 
